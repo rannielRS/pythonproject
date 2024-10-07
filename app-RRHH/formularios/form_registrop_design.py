5ab0480c990bf4857157ef4e438445daead258cf
Ing. Ranniel Rivero Sevila <ranniel.rivero@gmail.com>
2024-10-07 17:08:09 -0400

    Se modifico interfaz de registro de periodo para agregarle Anno y orden del periodo

--- app-RRHH/formularios/form_registrop_design.py
+++ app-RRHH/formularios/form_registrop_design.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 import pymssql
 from tkinter import ttk, messagebox
 from config import COLOR_CUERPO_PRINCIPAL, COLOR_BARRA_SUPERIOR
@@ -7,13 +8,29 @@ from config import COLOR_CUERPO_PRINCIPAL, COLOR_BARRA_SUPERIOR
 class FormularioRegistroPDesign():
 
     def __init__(self, panel_principal):           
+        #Definiendo variables
+        self.var_periodo = StringVar()
+        self.anno_trim = StringVar()
+        
         # Definiendo controles 
+
+        #comobo carga periodo del zun
         self.cbx_label = tk.Label(panel_principal, text="Período", bg=COLOR_CUERPO_PRINCIPAL)
         self.cbx_label.grid(row=1,column=0,padx=5,pady=20)
-        #comobo carga periodo del zun
-        self.cb_periodo = ttk.Combobox(panel_principal)
-        self.cb_periodo.grid(row=1,column=1,padx=5,pady=20)        
         
+        self.cb_periodo = ttk.Combobox(panel_principal,textvariable=self.var_periodo, postcommand=self.cargarcombo)
+        self.cb_periodo.grid(row=1,column=1,padx=5,pady=20)  
+        
+
+        #comobo carga de orden
+        self.cbx_labelO = tk.Label(panel_principal, text="Orden", bg=COLOR_CUERPO_PRINCIPAL)
+        self.cbx_labelO.grid(row=1,column=2,padx=5,pady=20)
+        
+        self.cb_orden = ttk.Combobox(panel_principal,postcommand=self.cargarcombo,values=['1','2','3'])
+        self.cb_orden.current(0)
+        self.cb_orden.grid(row=1,column=3,padx=5,pady=20) 
+
+        #nombre del periodo
         self.tx_label = tk.Label(panel_principal, text="Trimestre", bg=COLOR_CUERPO_PRINCIPAL)
         self.tx_label.grid(row=0,column=0,padx=5,pady=20)
         
@@ -21,12 +38,21 @@ class FormularioRegistroPDesign():
             'Times', 14), width=20)
         self.tx_trimestre_name.grid(row=0,column=1,padx=5,pady=20)
 
+        #Año del periodo
+        self.tx_labelA = tk.Label(panel_principal, text="Año", bg=COLOR_CUERPO_PRINCIPAL)
+        self.tx_labelA.grid(row=0,column=2,padx=5,pady=20)
+        
+        self.annot = str(self.var_periodo)[:4]
+
+        self.tx_anno = ttk.Entry(panel_principal, font=(
+            'Times', 14), width=20)
+        self.tx_anno.grid(row=0,column=3,padx=5,pady=20)
+
         #Boton para agregar periodo al treewiew
         self.btn_registro_periodo = tk.Button(panel_principal, text="Registrar", font=(
             'Times', 13), bg=COLOR_BARRA_SUPERIOR, bd=0, fg=COLOR_CUERPO_PRINCIPAL, padx=15, command="")
-        self.btn_registro_periodo.grid(row=1,column=2,padx=5,pady=20)
-        #self.btn_registro_periodo.bind(
-        #    "<Return>", (lambda event: self.registrar_producto()))
+        self.btn_registro_periodo.grid(row=1,column=4,padx=5,pady=20)
+        
 
 
         #Definiendo tree view de periodo
@@ -43,9 +69,12 @@ class FormularioRegistroPDesign():
         self.tree.heading('Orden', text='Orden')
         self.tree.grid(row=2,column=0, columnspan=4,padx=20,pady=20)
 
-        self.combo()
+        
 
-    def combo(self):   
+    def cargarcombo(self): 
+        self.anno_trim=self.tx_anno.get()  
+        
+        options=[]
         try:
             conn = pymssql.connect(
             server='10.105.213.6',
@@ -55,13 +84,21 @@ class FormularioRegistroPDesign():
             as_dict=True
             )
             cursor = conn.cursor()
-            cursor.execute('SELECT id_peri, nombre  FROM ZUNpr.dbo.n_periodo')
+            if self.anno_trim:
+                cursor.execute("SELECT id_peri,nombre,fecha_inicio  FROM ZUNpr.dbo.n_periodo where fecha_inicio like '%"+self.anno_trim+"%'")
+            else:
+                cursor.execute('SELECT id_peri,nombre,fecha_inicio  FROM ZUNpr.dbo.n_periodo')
             slist = cursor.fetchall()
-            
-            self.cb_periodo['value']=tuple(slist)
+            for row in slist:
+                options.append(str(row['id_peri'])+"-"+str(row['nombre']).rstrip()+"-"+str(row['fecha_inicio'])[:4])  
+
+            self.cb_periodo['value']=options
+
         except Exception as err:
             messagebox.showerror("Error",err)
 
+    
 
+    
 
     
